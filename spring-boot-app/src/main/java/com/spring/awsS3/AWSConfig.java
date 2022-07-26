859f64ab4035e24318b0ca4e5dd335849eb240c3
Shubham Yadav <46105856+Shubham-yadav36@users.noreply.github.com>
2022-07-26 13:08:00 +0530

    configured db and aws

--- spring-boot-app/src/main/java/com/spring/awsS3/AWSConfig.java
+++ spring-boot-app/src/main/java/com/spring/awsS3/AWSConfig.java
@@ -1,5 +1,6 @@
 package com.spring.awsS3;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,121 +24,116 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
-import java.io.ByteArrayInputStream;
-
-import lombok.extern.log4j.Log4j2;
 
 @Component
-@Log4j2
 public class AWSConfig {
 
-    private AmazonS3 amazonS3;
+	private AmazonS3 amazonS3;
 
-    @Value("${aws.accessKey}")
-    private String accessKey;
+	@Value("${aws.accessKey}")
+	private String accessKey;
 
-    @Value("${aws.secretKey}")
-    private String secretKey;
+	@Value("${aws.secretKey}")
+	private String secretKey;
 
-    @Value("${aws.bucketName}")
-    private String bucketName;
+	@Value("${aws.bucketName}")
+	private String bucketName;
 
-    private String domain = "http://localhost:8080/image/";
+	@Value("${aws.domain}")
+	private String domain;
 
-    @PostConstruct
-    public void initializeAws() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        amazonS3 = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.US_EAST_2)
-                .build();
-    }
+	@PostConstruct
+	public void initializeAws() {
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		amazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(Regions.US_EAST_2).build();
+	}
 
-    public String uploadImage(MultipartFile multipartFile) {
-        File file = convertMultipartToFile(multipartFile);
-        String fileName = System.currentTimeMillis() + "_" + file.getName();
-        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file));
-        file.delete();
-        return "savedFile : " + domain + fileName;
-    }
+	public String uploadImage(MultipartFile multipartFile) {
+		File file = convertMultipartToFile(multipartFile);
+		String fileName = System.currentTimeMillis() + "_" + file.getName();
+		amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file));
+		file.delete();
+		return "savedFile : " + domain + fileName;
+	}
 
-    public ImageResponse getFileFromS3Bucket(String fileName) {
-        try {
-            S3Object object = amazonS3.getObject(bucketName, fileName);
-            System.out.println("image : " + object.getKey());
-            try (S3ObjectInputStream stream = object.getObjectContent()) {
-                ByteArrayOutputStream temp = new ByteArrayOutputStream();
-                IOUtils.copy(stream, temp);
-                InputStream is = new ByteArrayInputStream(temp.toByteArray());
-                return new ImageResponse(is, object.getObjectMetadata().getContentType(),
-                        object.getObjectMetadata().getContentLength());
-            }
-        } catch (SdkClientException | IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
+	public ImageResponse getFileFromS3Bucket(String fileName) {
+		try {
+			S3Object object = amazonS3.getObject(bucketName, fileName);
+			System.out.println("image : " + object.getKey());
+			try (S3ObjectInputStream stream = object.getObjectContent()) {
+				ByteArrayOutputStream temp = new ByteArrayOutputStream();
+				IOUtils.copy(stream, temp);
+				InputStream is = new ByteArrayInputStream(temp.toByteArray());
+				return new ImageResponse(is, object.getObjectMetadata().getContentType(),
+						object.getObjectMetadata().getContentLength());
+			}
+		} catch (SdkClientException | IOException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-    public byte[] getImageFromS3(String fileName) {
-        try {
-            S3Object object = amazonS3.getObject(bucketName, fileName);
-            System.out.println("image : " + object.getKey());
-            return object.getObjectContent().readAllBytes();
-        } catch (SdkClientException | IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
+	public byte[] getImageFromS3(String fileName) {
+		try {
+			S3Object object = amazonS3.getObject(bucketName, fileName);
+			System.out.println("image : " + object.getKey());
+			return object.getObjectContent().readAllBytes();
+		} catch (SdkClientException | IOException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-    public String deleteImageFromS3(String fileName) {
-        amazonS3.deleteObject(bucketName, fileName);
-        return "deleted file : " + fileName;
-    }
+	public String deleteImageFromS3(String fileName) {
+		amazonS3.deleteObject(bucketName, fileName);
+		return "deleted file : " + fileName;
+	}
 
-    private File convertMultipartToFile(MultipartFile multipartFile) {
-        File file = new File(multipartFile.getOriginalFilename());
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(multipartFile.getBytes());
+	private File convertMultipartToFile(MultipartFile multipartFile) {
+		File file = new File(multipartFile.getOriginalFilename());
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(multipartFile.getBytes());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return file;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
 
-    public class ImageResponse {
+	public class ImageResponse {
 
-        private InputStream inputStream;
-        private String contentType;
-        private Long contentSize;
+		private InputStream inputStream;
+		private String contentType;
+		private Long contentSize;
 
-        public ImageResponse(InputStream inputStream, String contentType, Long contentSize) {
-            super();
-            this.inputStream = inputStream;
-            this.contentType = contentType;
-            this.contentSize = contentSize;
-        }
+		public ImageResponse(InputStream inputStream, String contentType, Long contentSize) {
+			super();
+			this.inputStream = inputStream;
+			this.contentType = contentType;
+			this.contentSize = contentSize;
+		}
 
-        public InputStream getInputStream() {
-            return inputStream;
-        }
+		public InputStream getInputStream() {
+			return inputStream;
+		}
 
-        public void setInputStream(InputStream inputStream) {
-            this.inputStream = inputStream;
-        }
+		public void setInputStream(InputStream inputStream) {
+			this.inputStream = inputStream;
+		}
 
-        public String getContentType() {
-            return contentType;
-        }
+		public String getContentType() {
+			return contentType;
+		}
 
-        public void setContentType(String contentType) {
-            this.contentType = contentType;
-        }
+		public void setContentType(String contentType) {
+			this.contentType = contentType;
+		}
 
-        public Long getContentSize() {
-            return contentSize;
-        }
+		public Long getContentSize() {
+			return contentSize;
+		}
 
-        public void setContentSize(Long contentSize) {
-            this.contentSize = contentSize;
-        }
-    }
+		public void setContentSize(Long contentSize) {
+			this.contentSize = contentSize;
+		}
+	}
 }
--- spring-boot-app/src/test/java/com/spring/SpringBootAppApplicationTests.java
+++ spring-boot-app/src/test/java/com/spring/SpringBootAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.spring;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringBootAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private Integer port;
+
+	private static RestTemplate restTemplate;
+
+	@BeforeAll
+	public static void init() {
+		restTemplate = new RestTemplate();
 	}
 
+	@Test
+	public void testAPI() {
+		System.out.println("API Testing");
+	}
 }
